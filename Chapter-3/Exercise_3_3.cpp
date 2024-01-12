48cb3c39e7e9dcac1526f107a22d3af4ac376dd5
Vaishnav Barbade <76620267+VaishnavPatil@users.noreply.github.com>
2024-01-12 23:51:50 +0530

    write a program to print following output using for loops 1 22 333 4444 55555
    This C++ program prompts the user to input the size of a pattern and then prints a triangular pattern of numbers. The numbers in each row are repeated based on the row number. However, there is an error in the nested loop condition, where i is mistakenly used instead of j. The corrected version ensures the proper printing of the desired pattern.

---
+++ Chapter-3/Exercise_3_3.cpp
@@ -0,0 +1,20 @@
+//
+// Created by vaishnav on 12-01-2024.
+//
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"\nDetermine tha size of pattern:- ";
+    cin>>n;
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; i<=n; j++)
+        {
+            cout<<i;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
